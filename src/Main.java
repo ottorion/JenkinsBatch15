8185edc8ff160dbc42112b0697ef1b6f3e97f24d
ottorion <ozandogusdal@gmail.com>
2023-05-30 20:16:52 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) throws InterruptedException{
+
+      for (int i = 0; i < 10; i++) {
+          System.out.println("Hello World");
+          Thread.sleep(2000);
+
+
+      }
+
+
+
+  }
+
+
+}
